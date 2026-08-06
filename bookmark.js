b55dfa62a1508e87175be5946bb2aba56e4de2bc
Aravind Adithiya <aravindadithiya@Aravinds-Mac-mini.local>
2026-08-06 19:54:05 +0530

    local storage pending

--- bookmark.js
+++ bookmark.js
@@ -1,29 +1,49 @@
-const bookmarkName = document.getElementById("Bookmark-Name")
-const bookmarkURL = document.getElementById("Bookmark-URL")
-const addbookmark = document.getElementById("btn")
-const container = document.querySelector(".content")
+const bookmarkName = document.getElementById("Bookmark-Name");
+const bookmarkURL = document.getElementById("Bookmark-URL");
+const addbookmark = document.getElementById("btn");
+const container = document.querySelector(".content");
 
+let bookmark = JSON.parse(localStorage.getItem("bookmark")) || [];;
 
-addbookmark.addEventListener("click",()=>{
+addbookmark.addEventListener("click", () => {
+  const div = document.createElement("div");
+  const link = document.createElement("a");
+  const btndelete = document.createElement("btn");
+  div.classList.add("add-bookmark");
+  btndelete.classList.add("btn-1");
+  link.href = bookmarkURL.value;
+  link.target = "_blank";
+  link.rel = "noopener noreferrer";
+  link.textContent = bookmarkName.value;
+  btndelete.textContent = "Remove";
+  div.append(link, btndelete);
+  container.append(div);
+  console.log(div);
+  bookmark.push({
+    name: bookmarkName.value,
+    url: bookmarkURL.value,
+  });
+  localStorage.setItem("bookmark", JSON.stringify(bookmark));
+console.log(localStorage.getItem("bookmark"));
+  bookmarkName.value = "";
+  bookmarkURL.value = "";
 
+  btndelete.addEventListener("click", () => {
+    div.remove();
+    console.log(bookmarkName.value);
+    console.log(bookmarkURL.value);
+  });
 
-  const div = `<div class="add-bookmark">
-          <a href= ${bookmarkURL.value} target="_blank" rel="noopener noreferrer">${bookmarkName.value}</a>
-      <button id="btn-1">Remove</button>
-    </div>`
-    console.log(bookmarkName.value)
-    console.log(bookmarkURL.value)
+  //  console.log(bookmarkName.value)
+  //  console.log(bookmarkURL.value)
 
-   container.insertAdjacentHTML("afterend",div)
-      
-})
+  // container.insertAdjacentHTML("afterend",div)
+});
 
-const deletebtn = document.getElementById("btn-1")
+// const deletebtn = document.getElementById("btn-1");
 
-deletebtn.addEventListener("click",(event)=>{
-   const selectdbtn = event.target.parentElement;
-   selectdbtn.remove() ;
-   console.log(selectdbtn)
-  
-
-})
+// deletebtn.addEventListener("click", (event) => {
+//   const selectdbtn = event.target.parentElement;
+//   selectdbtn.remove();
+//   console.log(selectdbtn);
+// });
