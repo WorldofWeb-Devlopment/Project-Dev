9baae5b010b4e5a35695d5c923f06c619af365dc
mm lab 40 <aravindbusiness7@gmail.com>
2026-08-08 12:24:09 +0530

    passwor Genearate Sucessfully

--- Password-Generator/script.js
+++ Password-Generator/script.js
@@ -10,24 +10,67 @@ const passwordBtn = document.getElementById("password-btn");
 const indicatorBar = document.getElementById("indicator-bar");
 const passwordStrength = document.getElementById("password-strength");
 
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numberCharacters = "0123456789";
+const symbolCharacters = "!@#$%^&*()-_=+[]{}|;:,.<>?/";
 
-silderControl.addEventListener("input",() => {
-    sliderLength.textContent = silderControl.value;
-})
+silderControl.addEventListener("input", () => {
+  sliderLength.textContent = silderControl.value;
+});
 
-passwordBtn.addEventListener("click", checkpassword)
+passwordBtn.addEventListener("click", checkpassword);
 
 function checkpassword() {
-    const length = Number(silderControl.value);
-    const hasUpperCaseChecked = upperCase.checked;
-    const hasLowerCaseChecked = lowerCase.checked;
-    const hasnumbersChecked = numbers.checked;
-    const hassymbolsChecked = symbols.checked;
+  const length = Number(silderControl.value);
+  const hasUpperCaseChecked = upperCase.checked;
+  const hasLowerCaseChecked = lowerCase.checked;
+  const hasnumbersChecked = numbers.checked;
+  const hassymbolsChecked = symbols.checked;
 
-    if (!hasUpperCaseChecked &&
-        !hasLowerCaseChecked &&
-        !hasnumbersChecked &&
-        !hassymbolsChecked) {
-         alert("Pleasen Check Any CheckBox")
+  if (
+    !hasUpperCaseChecked &&
+    !hasLowerCaseChecked &&
+    !hasnumbersChecked &&
+    !hassymbolsChecked
+  ) {
+    alert("Pleasen Check Any CheckBox");
+  }
+
+  const newPassword = createRandomPassword(
+    length,
+    hasUpperCaseChecked,
+    hasLowerCaseChecked,
+    hasnumbersChecked,
+    hassymbolsChecked,
+  );
+
+  inputbox.value = newPassword;
+
+  function createRandomPassword(
+    length,
+    hasUpperCaseChecked,
+    hasLowerCaseChecked,
+    hasnumbersChecked,
+    hassymbolsChecked,
+  ) {
+    let allCharacters = "";
+
+    if (hasUpperCaseChecked)  allCharacters+= uppercaseLetters;
+    if (hasLowerCaseChecked)  allCharacters+= lowercaseLetters;
+    if (hasnumbersChecked)  allCharacters+= numberCharacters;
+    if (hassymbolsChecked)  allCharacters+= symbolCharacters;
+
+    let password=""; 
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex  = Math.floor(Math.random()*allCharacters.length) 
+        password+=allCharacters[randomIndex]
+        
+        console.log(password)
+        console.log(allCharacters)
     }
+    return password;
+
+  }
 }
